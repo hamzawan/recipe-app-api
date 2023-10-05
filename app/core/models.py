5cc29e4049de017b1b535da79aeb97ac84799189
hamzawan <ah.awan33@gmail.com>
2023-10-05 21:34:39 +0500

    feat: User model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,42 @@
-from django.db import models # noqa
+from django.db import models  # noqa
 
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for user."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create save and return a new user."""
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create save and return a new super user."""
+        super_user = self.model(
+            email=self.normalize_email(email), **extra_fields)
+        super_user.set_password(password)
+        super_user.is_active = True
+        super_user.is_staff = True
+        super_user.is_superuser = True
+        super_user.save(using=self._db)
+        return super_user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
